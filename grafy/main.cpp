0fcc224aeffbbfda07ddf828e55c9eeaa5e950dc
pananananas <drumpixtv@gmail.com>
2022-05-08 19:27:52 +0200

    Initial Commit

---
+++ grafy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  grafy
+//
+//  Created by Eryk Wójcik on 08/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
